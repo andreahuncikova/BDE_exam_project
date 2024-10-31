a07cf35d8b34c05e72d85fdb0de8aaf4067ee326
andreahuncikova <andhun01@easv365.dk>
2024-10-31 23:35:13 +0100

    changed test

--- test/todo-app.js
+++ test/todo-app.js
@@ -3,8 +3,18 @@ import { Selector } from 'testcafe';
 fixture ( 'ToDo app tests' )
     .page ( 'test.andreahuncikova.com/todo' );
 
-    test('Test case 1', async t => {
+    test('User can add a new todo item', async t => {
+        // Selectors for elements on the page
+        const todoInput = Selector('#todo-input');
+        const todoForm = Selector('.todo-form');
+        const todoList = Selector('#todo-list');
+        const todoItem = todoList.find('.todo-item'); // Selects a new todo item in the list
+    
+        // Type a new todo item and submit the form
         await t
-            .click(Selector('#myButton'))
-            .expect(Selector('#myElement').exists).ok();
+            .typeText(todoInput, 'My new todo item')
+            .pressKey('enter'); // Or .click(todoForm) if form submission requires a click
+    
+        // Check if the todo item was added to the list
+        await t.expect(todoItem.withText('My new todo item').exists).ok();
     });
