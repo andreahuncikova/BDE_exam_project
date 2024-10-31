2e83b617549e7a52d7850df55ab304b2928fcd66
andreahuncikova <andhun01@easv365.dk>
2024-10-31 23:44:59 +0100

    testing

--- test/todo-app.js
+++ test/todo-app.js
@@ -3,12 +3,10 @@ import { Selector } from 'testcafe';
 fixture ( 'ToDo app tests' )
     .page ( 'test.andreahuncikova.com/todo' );
 
-    test('Check if at least one todo item exists', async t => {
-        const todoList = Selector('#todo-list');
+    test('Check if Clear Completed button exists', async t => {
+        // Select the button by its ID
+        const clearCompletedButton = Selector('#clearCompletedBtn');
     
-        // Check if there is at least one todo item in the list
-        const todoItemsCount = await todoList.child('li.todo-item').count;
-    
-        // Assert that the count of todo items is greater than 0
-        await t.expect(todoItemsCount).gt(0, 'There are no todo items in the list');
+        // Assert that the button exists
+        await t.expect(clearCompletedButton.exists).ok('The Clear Completed button does not exist');
     });
