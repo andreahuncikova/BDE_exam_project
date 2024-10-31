3317bc7b846c33f51bd1da0f1bb7925be2cc7b9f
andreahuncikova <andhun01@easv365.dk>
2024-10-31 23:31:50 +0100

    added test

--- test/todo-app.js
+++ test/todo-app.js
@@ -2,3 +2,9 @@ import { Selector } from 'testcafe';
 
 fixture ( 'ToDo app tests' )
     .page ( 'test.andreahuncikova.com/todo' );
+
+    test('Test case 1', async t => {
+        await t
+            .click(Selector('#myButton'))
+            .expect(Selector('#myElement').exists).ok();
+    });
