2bff567766ecb20c339a2feb610b46d0a29cd790
andreahuncikova <andhun01@easv365.dk>
2024-11-03 12:44:59 +0100

    changed due date test

--- test/todo-app.js
+++ test/todo-app.js
@@ -3,13 +3,15 @@ import { Selector } from 'testcafe';
 fixture ( 'ToDo app tests' )
     .page ( 'test.andreahuncikova.com/todo' );
 
-    test('Due Date Highlighting Test', async t => {
+    test('Add Todo with Due Date', async t => {
         const todoInput = Selector('#todo-input');
+        const dueDateInput = Selector('#due-date');
         const addButton = Selector('.todo-form button');
-        const todoItem = Selector('.todo-item').withText('Test todo');
+        const todoItem = Selector('.todo-item').withText('Test todo with due date');
       
         await t
-          .typeText(todoInput, 'Test todo with due date')
-          .click(addButton)
-          .expect(todoItem.exists).ok(); // Confirms the todo item with a due date is added
-      });
+            .typeText(todoInput, 'Test todo with due date') // Add the todo text
+            .typeText(dueDateInput, '2024-12-01') // Set the due date
+            .click(addButton) // Click the add button
+            .expect(todoItem.exists).ok(); // Check if the todo item was added successfully
+    });
