f9ca695023b6a5269842f7789f06b5c3f551d4bf
andreahuncikova <andhun01@easv365.dk>
2024-11-04 01:00:08 +0100

    final test

--- test/todo-app.js
+++ test/todo-app.js
@@ -5,26 +5,33 @@ fixture ( 'ToDo app tests' )
 
     // Test to add a new todo with due date
     test('Add Todo with Due Date', async t => {
+        // Arrange
         const todoInput = Selector('#todo-input');
         const dueDateInput = Selector('#due-date');
         const addButton = Selector('.todo-form button');
         const todoItem = Selector('.todo-item').withText('Test todo with due date');
       
+        // Act
         await t
             .typeText(todoInput, 'Test todo with due date') 
             .typeText(dueDateInput, '2024-12-01') 
             .click(addButton) 
+
+        // Assert
             .expect(todoItem.exists).ok(); 
     });
 
     // Test to toggle dark mode
     test('Toggle Dark Mode', async t => {
+        // Arrange
         const darkModeToggle = Selector('#darkModeToggle');
         const body = Selector('body');
 
+        // Act and Assert
         await t
             .click(darkModeToggle) 
             .expect(body.hasClass('dark-mode')).ok('Dark mode should be enabled')
+
             .click(darkModeToggle) 
             .expect(body.hasClass('dark-mode')).notOk('Dark mode should be disabled'); 
     });
